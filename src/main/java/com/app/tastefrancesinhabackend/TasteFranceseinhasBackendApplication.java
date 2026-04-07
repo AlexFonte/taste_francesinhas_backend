e7fd6101e1c0bc35076a74e62472dd31ea994a22
Alex Fonte <aramosdf@gmail.com>
2026-04-07 23:09:33 +0200

    Fisrt commit
    Signed-off-by: Alex Fonte <aramosdf@gmail.com>

---
+++ src/main/java/com/app/tastefrancesinhabackend/TasteFranceseinhasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.app.tastefrancesinhabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasteFranceseinhasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasteFranceseinhasBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/tastefrancesinhabackend/TasteFranceseinhasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tastefrancesinhabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasteFranceseinhasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
