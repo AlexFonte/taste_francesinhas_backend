82fdcdb73ca62cc225764eaf8421d98f36a854ce
Alex Fonte <aramosdf@gmail.com>
2026-05-05 23:23:05 +0200

    Fix: favorite dto
    Signed-off-by: Alex Fonte <aramosdf@gmail.com>

--- src/main/java/com/app/tastefrancesinhasbackend/dto/FavoriteDTO.java
+++ src/main/java/com/app/tastefrancesinhasbackend/dto/FavoriteDTO.java
@@ -1,17 +1,14 @@
 package com.app.tastefrancesinhasbackend.dto;
 
 import com.app.tastefrancesinhasbackend.entity.Favorite;
-import com.app.tastefrancesinhasbackend.entity.enums.FrancesinhaType;
 
 import java.time.LocalDateTime;
 
 public class FavoriteDTO {
 
     public record FavoriteResponse(
-            Long francesinhaId,
-            String francesinhaName,
-            FrancesinhaType francesinhaType,
-            String restaurantCity,
+            Long id,
+            FrancesinhaDTO.FrancesinhaResponse francesinha,
             LocalDateTime createdAt
     ) {}
 
@@ -20,13 +17,10 @@ public class FavoriteDTO {
             Long francesinhaId
     ) {}
 
-    // Mapea la entidad Favorite al record de respuesta
     public static FavoriteResponse response(Favorite f) {
         return new FavoriteResponse(
-                f.getFrancesinha().getId(),
-                f.getFrancesinha().getName(),
-                f.getFrancesinha().getType(),
-                f.getFrancesinha().getRestaurant().getCity(),
+                f.getId(),
+                FrancesinhaDTO.responsePublic(f.getFrancesinha()),
                 f.getCreatedAt()
         );
     }
